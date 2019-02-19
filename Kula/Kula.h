1c672b9a3828d08031cd938a951747d0cc9b8f08
Jason Beck <mrbass21@gmail.com>
2019-02-19 17:14:00 -0600

    Initial Commit

---
+++ Kula/Kula.h
@@ -0,0 +1,19 @@
+//
+//  Kula.h
+//  Kula
+//
+//  Created by Jason Beck on 2/19/19.
+//  Copyright © 2019 Jason Beck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kula.
+FOUNDATION_EXPORT double KulaVersionNumber;
+
+//! Project version string for Kula.
+FOUNDATION_EXPORT const unsigned char KulaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kula/PublicHeader.h>
+
+
